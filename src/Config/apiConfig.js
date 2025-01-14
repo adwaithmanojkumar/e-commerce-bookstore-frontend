6b228c672694164260ef52b371fc0c362062d305
adwaith <adwaithmanojkumar@gmail.com>
2025-01-14 07:22:48 +0530

    added origin url

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = 'http://localhost:8080'
+export const API_BASE_URL = 'https://fulfilling-tranquility-production.up.railway.app'
 const jwt = localStorage.getItem("jwt")
 
 export const api = axios.create({
